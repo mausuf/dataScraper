89ac9d128b8f19b54ba4c3e322b44514e7f60939
starbuck <mausuf@utexas.edu>
2019-05-07 15:47:20 -0500

    update 2 for routes.js

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,10 @@
+// Bring in scrape function from scripts directory
+var scrape = require("../scripts/scrape");
+
+// Bring in headlines and notes from the controller
+var headlinesController = require("../controllers/headlines");
+var notesController = require("../controllers/notes");
+
 module.exports = function(router) {
 // Home Page Handlebars Router
 router.get("/", function(req,res) {
@@ -7,4 +14,66 @@ router.get("/", function(req,res) {
 router.get("/saved", function(req,res){
     res.render("saved");
 });
+
+router.get("/api/fetch", function(req, res) {
+    headlinesController.fetch(function(err, docs) {
+        if (!docs || docs.insertedCount === 0) {
+            res.json({
+                message: "No new articles to scrape at this time!"
+            });
+        } else {
+            res.json({
+                message: "Added" + docs.insertedCount + "new articles!"
+                });
+            }
+        });
+    });
+router.get("/api/headlines", function(req, res) {
+    var query = {};
+    if (req.query.saved) {
+        query = req.query;
+    }
+
+    headlinesController.get(query, function(data) {
+        res.json(data);
+    });
+});
+
+router.delete("/api/headlines/:id", function(req, res) {
+    var query = {};
+    query._id = req.params.id;
+    headlinesController.delete(query, function(err, data) {
+        res.json(data);
+    });
+});
+
+router.patch("/api/headlines", function(req, res) {
+    headlinesController.update(req.body, function(err, data) {
+        res.json(data);
+    });
+});
+
+router.get("api/notes/:headline_id?", function(req, res) {
+    var query = {};
+    if (req.params.headline_id) {
+        query._id = req.params.headline_id;
+    }
+    notesController.get(query, function(err, data) {
+        res.json(data);
+    });
+});
+
+router.delete("/api/notes/:id", function(req, res) {
+    var query = {};
+    query._id = req.params.id;
+    notesController.delete(query, function(err, data) {
+        res.json(data);
+    });
+});
+
+router.post("/api/notes", function(req, res) {
+    notesController.save(req.body, function(data) {
+        res.json(data);
+    });
+});     
 }
